d6fb4188e56e8611059eb36bb7449b2b35d0c3fe
xuena <xuena@xuenas-iMac.local>
2018-05-20 16:45:59 -0700

    first commit for the files

---
+++ media.py
@@ -0,0 +1,9 @@
+class Video :
+	def __init__(self, title, storyline) :
+		self.title = title
+		self.storyline = storyline
+class Movie(Video) :
+	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url) :
+		Video.__init__(self, title, storyline)
+		self.trailer_youtube_url = trailer_youtube_url
+		self.poster_image_url = poster_image_url
