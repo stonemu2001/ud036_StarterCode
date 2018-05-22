6da3c2aa63c74609e493bebe80d94061088d3436
Bingmo Jin <bingmo@gmail.com>
2018-05-22 14:59:35 -0700

    Fixed with correct code format.

--- media.py
+++ media.py
@@ -1,12 +1,13 @@
-#Video class 
-class Video :
-	def __init__(self, title, storyline) :
+# Video class
+class Video:
+	def __init__(self, title, storyline):
 		self.title = title
 		self.storyline = storyline
 
-#Movie class inherits Video class
-class Movie(Video) :
-	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url) :
+
+# Movie class inherits Video class
+class Movie(Video):
+	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
 		Video.__init__(self, title, storyline)
 		self.trailer_youtube_url = trailer_youtube_url
 		self.poster_image_url = poster_image_url
